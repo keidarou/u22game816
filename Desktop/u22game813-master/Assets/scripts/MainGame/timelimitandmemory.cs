78f636f81b5a3b1cc0a3932dea57a16f1cfaabb4
tamaron1203 <tamaron1203@makkubukku.local>
2017-08-15 15:05:18 +0900

    wei

---
+++ Desktop/u22game813-master/Assets/scripts/MainGame/timelimitandmemory.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class timelimitandmemory : MonoBehaviour
+{
+
+    public int clearcount = 0;
+    public Texture2D tex;
+    public Font f;
+    private GUIStyle labelStyle;
+    private GUIStyle Highscore;
+    public bool gameoverflag = false;
+    float timer;
+    movetheballautomatic script;
+    public GameObject movetheballl;
+    public void zenkaivoid()
+    {
+        clearcount++;
+        Debug.Log(clearcount);
+    }
+    static timelimitandmemory _instance = null;
+
+    // オブジェクト実体の参照（初期参照時、実体の登録も行う）
+    static timelimitandmemory instance
+    {
+        get { return _instance ?? (_instance = FindObjectOfType<timelimitandmemory>()); }
+    }
+
+    void Awake()
+    {
+        if (this != instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 以降破棄しない
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void OnDestroy()
+    {
+        if (this == instance) _instance = null;
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+       
+        this.labelStyle = new GUIStyle();
+        this.labelStyle.fontSize = Screen.height / 20;
+        this.labelStyle.normal.textColor = Color.white;
+        Highscore = new GUIStyle();
+        Highscore.fontSize = Screen.height / 15;
+        Highscore.normal.textColor = Color.red;
+        labelStyle.font = f; Highscore.font = f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movetheballl = GameObject.Find("MoveTheBall");
+        script = movetheballl.GetComponent<movetheballautomatic>();
+        if (script.pauseflag == false)
+        {
+            if (timer > 100) { gameoverflag = true; }
+            else
+            {
+                timer += Time.fixedDeltaTime;
+            }
+        }
+
+    }
+    void OnGUI()
+    {
+        string s = string.Format("{0}Seconds Left", 100 - (int)timer);
+        string str = string.Format("Score : {0}", (int)clearcount);
+        GUI.Label(new Rect(Screen.width-300, Screen.height-100, 100, 30), str, labelStyle);
+        GUI.Label(new Rect(20, Screen.height-50, 100, 30), s, labelStyle);
+        if (gameoverflag == true)
+        {
+            int imamadenomax = PlayerPrefs.GetInt("highscore", 0);
+            if (imamadenomax < clearcount)//もしハイスコアなら
+            {
+                GUI.Label(new Rect(Screen.width / 2, Screen.height, 300, 60), "Highscore!!", Highscore);//表示されない
+                string sscore = string.Format("Highscore : {0}    Your Score : {0}", (int)clearcount);
+                GUI.Label(new Rect(Screen.width / 2, Screen.height + 60, 500, 60), sscore, labelStyle);
+            }
+            else
+            {
+                string sscore = string.Format("Highscore : {0}    Your Score : {0}", (int)clearcount);
+                GUI.Label(new Rect(Screen.width / 2, Screen.height + 60, 500, 60), sscore, labelStyle);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Desktop/u22game813-master/Assets/scripts/Title/botanmanager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class botanmanager : MonoBehaviour
+{	
+    [SerializeField]
+    Animator animator;
+    //public GameObject leftbutton, rightbutton,
+	public GameObject camer;
+	public GameObject touch;
+
+	touch script;
+
+    void Start()
+    {
+		script = touch.GetComponent<touch> ();
+        animator = camer.GetComponent<Animator>();
+    }
+
+    public int muki = 0;
+	int touchinfo;
+
+	void Update(){
+		touchinfo = script.GetTouch ();
+		if (touchinfo == 2) {
+			rightchange ();
+		}
+		if (touchinfo == 1) {
+			leftchange ();
+		}
+		if (touchinfo == -1) {
+			mannnakabutton ();
+		}
+	}
+
+	public void leftchange(){
+        if (muki == 0)
+        {
+            //leftbutton.SetActive(false);
+            animator.SetTrigger("centertolefttrigger");
+            animator.SetInteger("where", 1);
+        }
+        if (muki == 1)
+        {
+            //rightbutton.SetActive(true);
+            animator.SetTrigger("righttocentertrigger");
+            animator.SetInteger("where", 4);
+        }
+        muki--;
+		if (muki == -2) {
+			muki = 2;
+		}
+    }
+    public void rightchange()
+    {
+        if (muki == 0)
+        {
+            //rightbutton.SetActive(false);
+            animator.SetTrigger("centertorighttrigger");
+            animator.SetInteger("where", 3);
+        }
+        if (muki == -1)
+        {
+            //leftbutton.SetActive(true);
+            animator.SetTrigger("lefttocentertrigger");
+            animator.SetInteger("where", 2);
+        }
+        muki++;
+
+    }
+    public void mannnakabutton()
+    {
+        if (muki == 0)
+        {
+            SceneManager.LoadScene("StageSelect");
+        }
+         if (muki == -1)
+         {
+             SceneManager.LoadScene("automaticselectlevel");
+         }
+    }
+
+
+}
